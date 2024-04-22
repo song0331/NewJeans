9b26a6254f2c0dc7f2d2b62ac31e832475e5c66d
song0331 <thdgusrb@gmail.com>
2024-04-22 21:26:59 +0900

    [Modify] profile 페이지 리팩토링

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -2,70 +2,29 @@ import setTitle from "../../lib/setTitle.js";
 
 setTitle("Profile");
 
-let cnt = 0;
+let showNav = false;
 $(".navbar-btn").click(function () {
-  cnt++;
-  if (cnt % 2 != 0) {
-    $(".navbar").addClass("open");
-    $(".navbar").removeClass("close");
-  } else {
-    $(".navbar").addClass("close");
-    $(".navbar").removeClass("open");
-  }
+  $(".navbar").toggleClass("open", !showNav);
+  $(".navbar").toggleClass("close", showNav);
+  showNav = !showNav;
 });
 
 $(".info").click(function (e) {
   $(e.target.nextElementSibling).toggleClass("text-opacity");
 });
 
-let cnt2 = 0;
+let changeImg = false;
 $(".btn").click(function () {
-  if (cnt2 % 2 == 0) {
-    $(".on").css("display", "block");
-    $(".off").css("display", "none");
-    $(".minji").css(
-      "background-image",
-      "url(https://song0331.github.io/newjeansImg/profile/after/0.jpg)"
-    );
-    $(".hanni").css(
-      "background-image",
-      "url(https://song0331.github.io/newjeansImg/profile/after/1.jpg)"
-    );
-    $(".danielle").css(
-      "background-image",
-      "url(https://song0331.github.io/newjeansImg/profile/after/2.jpg)"
-    );
-    $(".haerin").css(
-      "background-image",
-      "url(https://song0331.github.io/newjeansImg/profile/after/3.jpg)"
-    );
-    $(".hyein").css(
-      "background-image",
-      "url(https://song0331.github.io/newjeansImg/profile/after/4.jpg)"
-    );
-  } else {
-    $(".on").css("display", "none");
-    $(".off").css("display", "block");
-    $(".minji").css(
-      "background-image",
-      "url(https://song0331.github.io/newjeansImg/profile/before/0.jpg)"
-    );
-    $(".hanni").css(
-      "background-image",
-      "url(https://song0331.github.io/newjeansImg/profile/before/1.jpg)"
-    );
-    $(".danielle").css(
-      "background-image",
-      "url(https://song0331.github.io/newjeansImg/profile/before/2.jpg)"
-    );
-    $(".haerin").css(
-      "background-image",
-      "url(https://song0331.github.io/newjeansImg/profile/before/3.jpg)"
-    );
-    $(".hyein").css(
-      "background-image",
-      "url(https://song0331.github.io/newjeansImg/profile/before/4.jpg)"
-    );
-  }
-  cnt2++;
+  const imgURL = "https://song0331.github.io/newjeansImg/profile/";
+  const member = ["minji", "hanni", "danielle", "haerin", "hyein"];
+  const state = !changeImg ? "after" : "before";
+
+  $(".on").css("display", !changeImg ? "block" : "none");
+  $(".off").css("display", changeImg ? "block" : "none");
+
+  member.forEach((item, idx) =>
+    $(`.${item}`).css("background-image", `url(${imgURL}${state}/${idx}.jpg)`)
+  );
+
+  changeImg = !changeImg;
 });
