69e31924b30a159b665ad1d760b9cbae86aaa1d1
song0331 <thdgusrb@gmail.com>
2024-04-22 22:50:00 +0900

    [Modify] 배포 폴더 수정

---
+++ docs/assets/main-BOxjpGdl.js
@@ -0,0 +1,1 @@
+let a=!1;$(".navbar-btn").click(function(){$(".navbar").toggleClass("open",!a),$(".navbar").toggleClass("close",a),a=!a});
--- docs/assets/main-D3hfl2qC.js
+++
@@ -1,1 +0,0 @@
-let a=0;$(".navbar-btn").click(function(){a++,a%2!=0?($(".navbar").addClass("open"),$(".navbar").removeClass("close")):($(".navbar").addClass("close"),$(".navbar").removeClass("open"))});
