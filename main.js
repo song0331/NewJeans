3f2085462dcb36a7c163cbf080612f999a56faa3
song0331 <thdgusrb@gmail.com>
2024-05-12 18:00:23 +0900

    [Modify] navbar 기본 동작 막기, spaceBar로 이동 가능

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let showNav = false;
-$(".navbar-btn").click(function () {
+$(".navbar-btn").click(() => {
   $(".navbar").toggleClass("open", !showNav);
   $(".navbar").toggleClass("close", showNav);
   showNav = !showNav;
+});
+
+$("a").keypress((e) => {
+  e.preventDefault();
+  window.location.href = e.target.href;
 });
