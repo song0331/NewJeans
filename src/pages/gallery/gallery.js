8c06d7268c69c5d7058895e4510529146eaec858
song0331 <thdgusrb@gmail.com>
2024-05-12 17:42:36 +0900

    [Modify] async/await 사용으로 빌드 에러 해결

--- src/pages/gallery/gallery.js
+++ src/pages/gallery/gallery.js
@@ -3,42 +3,37 @@ import pb from "../../api/pocketbase.js";
 
 setTitle("Gallery");
 
-let showNav = false;
-$(".navbar-btn").click(function () {
-  $(".navbar").toggleClass("open", !showNav);
-  $(".navbar").toggleClass("close", showNav);
-  showNav = !showNav;
-});
+const rendering = async () => {
+  const galleryData = await pb.collection("gallery").getFullList();
 
-const galleryData = pb.collection("gallery").getFullList();
-
-galleryData.then((data) => {
-  data.forEach((item) => {
+  galleryData.forEach((item) => {
     let img = `
     <li class="image-list-item" tabindex="0">
-      <img class="box-item" src="${
-        import.meta.env.VITE_PB_API
-      }/api/files/gallery/${item.id}/${item.image}" alt="${item.year} 뉴진스" />
+    <img src="${import.meta.env.VITE_PB_API}/api/files/gallery/${item.id}/${
+      item.image
+    }" alt="${item.year} 뉴진스" />
     </li>
-  `;
+        `;
 
     $(".image-list").append(img);
   });
-});
 
-$(".image-list-item").click((e) => {
-  $(".modal")[0].show();
-  $(".modal").css("background-image", `url('${e.target.src}')`);
-});
+  $(".image-list-item").click((e) => {
+    $(".modal")[0].show();
+    $(".modal").css("background-image", `url(${e.target.src})`);
+  });
 
-$(".image-list-item").keypress((e) => {
-  e.preventDefault();
-  $(".modal")[0].show();
-  $(".modal").css("background-image", `url('${e.target.children[0].src}')`);
-});
+  $(".image-list-item").keypress((e) => {
+    e.preventDefault();
+    $(".modal")[0].show();
+    $(".modal").css("background-image", `url('${e.target.children[0].src}')`);
+  });
 
-$(".modal").click(() => $(".modal")[0].close());
+  $(".modal").click(() => $(".modal")[0].close());
 
-$("html").keydown((e) => {
-  if (e.keyCode === 27) $(".modal")[0].close();
-});
+  $("html").keydown((e) => {
+    if (e.keyCode === 27) $(".modal")[0].close();
+  });
+};
+
+rendering();
